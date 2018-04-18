c5f24a1fa4a3fd86ff440f14671009e1ad90856e
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-04-18 23:14:23 +0300

    Update Namespaces_in_Python.py

--- Namespaces_in_Python.py
+++ Namespaces_in_Python.py
@@ -1,49 +1,61 @@
+# File: Namespaces_in_Python.py
+# Description: Emulation of the work of the namespace in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Emulation of the work of the namespace in Python // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Namespaces_in_Python (date of access: XX.XX.XXXX)
+
+# Number of input lines
 n = int(input())
-scopes = {'global': {'funcs': [], 'vars': []}}
+spaces = {'global': {'features': [], 'variables': []}}
 
-def add(scopes, current_namespace, what):
-    if current_namespace not in scopes:
-        scopes[current_namespace] = {}
-        scopes[current_namespace]['vars'] = []
-        scopes[current_namespace]['vars'].append(what)
+def add(spaces, current_namespace, what):
+    if current_namespace not in spaces:
+        spaces[current_namespace] = {}
+        spaces[current_namespace]['variables'] = []
+        spaces[current_namespace]['variables'].append(what)
     else:
-        if 'vars' not in scopes[current_namespace]:
-            scopes[current_namespace]['vars'] = []
-            scopes[current_namespace]['vars'].append(what)
+        if 'variables' not in spaces[current_namespace]:
+            spaces[current_namespace]['variables'] = []
+            spaces[current_namespace]['variables'].append(what)
         else:
-            scopes[current_namespace]['vars'].append(what)
+            spaces[current_namespace]['variables'].append(what)
 
-def create(scopes, current_namespace, parent_namespace):
-    if current_namespace not in scopes:
-        scopes[current_namespace] = {}
-        scopes[current_namespace]['funcs'] = []
-        scopes[current_namespace]['vars'] = []
-        scopes[parent_namespace]['funcs'].append(current_namespace)
-        scopes[current_namespace]['parent'] = parent_namespace
+def create(spaces, current_namespace, parent_namespace):
+    if current_namespace not in spaces:
+        spaces[current_namespace] = {}
+        spaces[current_namespace]['features'] = []
+        spaces[current_namespace]['variables'] = []
+        spaces[parent_namespace]['features'].append(current_namespace)
+        spaces[current_namespace]['parent'] = parent_namespace
     else:
-        if 'funcs' not in scopes[current_namespace]:
-            scopes[current_namespace]['funcs'] = []
-            scopes[current_namespace]['parent'] = parent_namespace
-            scopes[parent_namespace]['funcs'].append(current_namespace)
+        if 'features' not in spaces[current_namespace]:
+            spaces[current_namespace]['features'] = []
+            spaces[current_namespace]['parent'] = parent_namespace
+            spaces[parent_namespace]['features'].append(current_namespace)
         else:
-            scopes[current_namespace]['funcs'].append(current_namespace)
-            scopes[parent_namespace]['funcs'].append(current_namespace)
+            spaces[current_namespace]['features'].append(current_namespace)
+            spaces[parent_namespace]['features'].append(current_namespace)
 
-def search(scopes, namespace, what):
-    if what in scopes[namespace]['vars']:
+def search(spaces, namespace, what):
+    if what in spaces[namespace]['variables']:
         return namespace
     else:
         try:
-            upper_namespace = scopes[namespace]['parent']
+            upper_namespace = spaces[namespace]['parent']
         except KeyError:
             return None
-        return search(scopes, upper_namespace, what)
+        return search(spaces, upper_namespace, what)
 
 for i in range(n):
     command = input().split()
     if command[0] == 'add':
-        add(scopes, command[1], command[2])
+        add(spaces, command[1], command[2])
     elif command[0] == 'create':
-        create(scopes, command[1], command[2])
+        create(spaces, command[1], command[2])
     elif command[0] == 'get':
-        print(search(scopes, command[1], command[2]))
+        print(search(spaces, command[1], command[2]))
